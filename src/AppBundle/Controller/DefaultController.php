5d8806d399d4a73ae2d69e72aa8a84d8c3993faa
Sébastien STELTZLEN <mail2sbeuh@gmail.com>
2017-12-14 19:40:18 +0100

    Ajout compteur

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -51,23 +51,6 @@ class DefaultController extends Controller
 
 
 
-                        // $nbParticipants = $_POST['event'.$event->getNbParticipants()];
-                        if (isset($_GET["participer"]))
-                        {
-                            $em = $this->getDoctrine()->getManager();
-                            $event = $em->getRepository('AppBundle:Event')->findId();
-                            var_dump($event);
-                        $nbParticipants=$event->getNbParticipants();
-                        var_dump($nbParticipants);
-                        $nbParticipants++;
-                        var_dump($nbParticipants);
-                        $event->setNbParticipants($nbParticipants);
-                        $em->persist($event);
-                        $em->flush();
-                        return $this->render('default/resultats2.html.twig', array(
-                            'events'=>$events,
-                        ));
-                        }
 
 
                 return $this->render('default/resultats.html.twig', array(
@@ -80,6 +63,8 @@ class DefaultController extends Controller
             'form' => $form->createView(),
         ));
     }
+
+
         /**
          * @Route("/monresultat/{id}", name="monresultat")
          * @Method("GET")
@@ -90,5 +75,34 @@ class DefaultController extends Controller
                 'event' => $event
             ));
       }
+
+
+          /**
+           * @Route("/monresultat/{id}", name="monresultat")
+           * @Method("GET")
+           */
+           public function updateAction(Request $request, Event $event)
+           {
+
+                   $em = $this->getDoctrine()->getManager();
+                   $events = $em->getRepository('AppBundle:Event')->findById($event);
+                // var_dump($event);
+               foreach($events as $event){
+                $nbParticipants = $event->getNbParticipants();
+
+                   if ($request->query->get('participer') == "J'y participe !"){
+                       $nbParticipants++;
+                       var_dump($nbParticipants);
+                       // var_dump($nbParticipants);
+                       $event->setNbParticipants($nbParticipants);
+                       $em->persist($event);
+                       $em->flush();
+                      }
+                }
+
+           return $this->render('default/resultats2.html.twig', array(
+               'event' => $event
+           ));
+        }
 
 }
